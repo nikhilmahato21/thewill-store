2777e9123901566727f155ce79341df83dfe3b94
nikhilmahato21 <nikhilmahato3337@gmail.com>
2025-04-13 13:30:06 +0530

    change member role

--- backend/src/controllers/workspace.controller.ts
+++ backend/src/controllers/workspace.controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import {
+  changeRoleSchema,
   createWorkspaceSchema,
   workspaceIdSchema,
 } from "../validation/workspace.validation";
 import { HTTPSTATUS } from "../config/http.config";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
 import {
+  changeMemberRoleService,
   createWorkspaceService,
   getAllWorkspacesUserIsMemberService,
+  getWorkspaceAnalyticsService,
   getWorkspaceByIdService,
   getWorkspaceMembersService,
 } from "../services/workspace.service";
@@ -74,3 +77,86 @@ export const getWorkspaceMembersController = asyncHandler(
     });
   }
 );
+
+export const getWorkspaceAnalyticsController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const workspaceId = workspaceIdSchema.parse(req.params.id);
+    const userId = req.user?._id;
+
+    const { role } = await getMemberRoleInWorkspace(userId, workspaceId);
+    roleGuard(role, [Permissions.VIEW_ONLY]);
+
+    const { analytics } = await getWorkspaceAnalyticsService(workspaceId);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Workspace analytics retrieved successfully",
+      analytics,
+    });
+  }
+);
+export const changeWorkspaceMemberRoleController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const workspaceId = workspaceIdSchema.parse(req.params.id);
+    const { memberId, roleId } = changeRoleSchema.parse(req.body);
+
+    const userId = req.user?._id;
+
+    const { role } = await getMemberRoleInWorkspace(userId, workspaceId);
+    roleGuard(role, [Permissions.CHANGE_MEMBER_ROLE]);
+
+    const { member } = await changeMemberRoleService(
+      workspaceId,
+      memberId,
+      roleId
+    );
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Member Role changed successfully",
+      member,
+    });
+  }
+);
+
+// export const updateWorkspaceByIdController = asyncHandler(
+//   async (req: Request, res: Response) => {
+//     const workspaceId = workspaceIdSchema.parse(req.params.id);
+//     const { name, description } = updateWorkspaceSchema.parse(req.body);
+
+//     const userId = req.user?._id;
+
+//     const { role } = await getMemberRoleInWorkspace(userId, workspaceId);
+//     roleGuard(role, [Permissions.EDIT_WORKSPACE]);
+
+//     const { workspace } = await updateWorkspaceByIdService(
+//       workspaceId,
+//       name,
+//       description
+//     );
+
+//     return res.status(HTTPSTATUS.OK).json({
+//       message: "Workspace updated successfully",
+//       workspace,
+//     });
+//   }
+// );
+
+// export const deleteWorkspaceByIdController = asyncHandler(
+//   async (req: Request, res: Response) => {
+//     const workspaceId = workspaceIdSchema.parse(req.params.id);
+
+//     const userId = req.user?._id;
+
+//     const { role } = await getMemberRoleInWorkspace(userId, workspaceId);
+//     roleGuard(role, [Permissions.DELETE_WORKSPACE]);
+
+//     const { currentWorkspace } = await deleteWorkspaceService(
+//       workspaceId,
+//       userId
+//     );
+
+//     return res.status(HTTPSTATUS.OK).json({
+//       message: "Workspace deleted successfully",
+//       currentWorkspace,
+//     });
+//   }
+// );
--- backend/src/routes/workspace.route.ts
+++ backend/src/routes/workspace.route.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import {
+  changeWorkspaceMemberRoleController,
   createWorkspaceController,
   getAllWorkspacesUserIsMemberController,
+  getWorkspaceAnalyticsController,
   getWorkspaceByIdController,
+  getWorkspaceMembersController,
 } from "../controllers/workspace.controller";
 
 const workspaceRoutes = Router();
 workspaceRoutes.post("/create/new", createWorkspaceController);
 workspaceRoutes.get("/all", getAllWorkspacesUserIsMemberController);
-workspaceRoutes.get("/members/:id", getWorkspaceByMembersController);
+workspaceRoutes.put(
+  "/change/member/role/:id",
+  changeWorkspaceMemberRoleController
+);
+workspaceRoutes.get("/members/:id", getWorkspaceMembersController);
+workspaceRoutes.get("/analytics/:id", getWorkspaceAnalyticsController);
 workspaceRoutes.get("/:id", getWorkspaceByIdController);
 export default workspaceRoutes;
